f4376b1f1b193904815e92f7f9c65a678603c052
Timothy Davis <tmthydvs@gmail.com>
2022-12-22 09:54:07 +0930

    Add alacritty detection

--- xdg_config/fish/config.fish
+++ xdg_config/fish/config.fish
@@ -1,4 +1,9 @@
 if status is-interactive
+    infocmp alacritty >/dev/null 2>/dev/null
+    if test $status -eq 0
+        set -x TERM "alacritty"
+    end
+
     # Commands to run in interactive sessions can go here
     if not functions -q fisher
         curl -sL https://git.io/fisher | source
@@ -17,5 +22,11 @@ if status is-interactive
     set_onedark $onedark_options
 end
 
+function fish_greeting
+    echo The time is (set_color yellow; date +%T; set_color normal)
+    echo
+end
+
 fish_add_path -m ~/.local/bin
 fish_add_path ~/.cargo/bin
+fish_add_path /usr/local/go/bin

607d559ebf9ce82ef95c51c8d28a8b7ebb8170c9
Timothy Davis <tmthydvs@gmail.com>
2022-12-27 22:22:40 +0930

    add smooth scroll and treesitter-context

--- xdg_config/nvim/lua/tmthy/plugins.lua
+++ xdg_config/nvim/lua/tmthy/plugins.lua
@@ -3,6 +3,17 @@ return {
     { 'kylechui/nvim-surround', version = '*', config = true },
     {
         'nvim-treesitter/nvim-treesitter-textobjects',
-        dependencies = { 'nvim-treesitter' }
+        dependencies = { 'nvim-treesitter' },
+    },
+    {
+        'nvim-treesitter/nvim-treesitter-context',
+        dependencies = { 'nvim-treesitter' },
+        config = true,
+    },
+    {
+        'karb94/neoscroll.nvim',
+        config = {
+            mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>', '<C-y>', '<C-e>' },
+        },
     },
 }

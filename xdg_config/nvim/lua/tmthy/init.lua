23b520d5bb4eaee7085a1624b87d859c20b833c4
Timothy Davis <tmthydvs@gmail.com>
2022-12-29 18:48:09 +0930

    Re-organise loading lazy

--- xdg_config/nvim/lua/tmthy/init.lua
+++ xdg_config/nvim/lua/tmthy/init.lua
@@ -1,8 +1,27 @@
+-- Need to be done very very early!
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.g.mapleader = ' '
+
 -- Vim options (vim.opt, etc.)
+--
+-- Set all vim options before plugin initialization
 require('tmthy.options')
 
--- Key rebinds that don't fit into plugin setups
-require('tmthy.keys')
+-- Load the plugins!
+require('tmthy.lazy')
 
--- AutoCmds...
-require('tmthy.autocmds')
+-- After plugins have loaded, load the other modules
+--
+-- VeryLazy is an event defined by Lazy.nvim, which
+-- fires after plugins have loaded.
+vim.api.nvim_create_autocmd('User', {
+    pattern = 'VeryLazy',
+    callback = function()
+        -- Key rebinds that don't fit into plugin setups
+        require('tmthy.keys')
+
+        -- AutoCmds...
+        require('tmthy.autocmds')
+    end
+})
+

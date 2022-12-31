b2bb554cd8bbea562a72be1af0747b43e62adda1
Timothy Davis <tmthydvs@gmail.com>
2022-12-31 18:55:24 +0930

    Remove nvim, moving to submodule

--- xdg_config/nvim/lua/tmthy/autocmds.lua
+++
@@ -1,9 +0,0 @@
-local api = vim.api
-
-local group = api.nvim_create_augroup('tmthy-augroup', { clear = true })
-api.nvim_create_autocmd('TextYankPost', {
-    group = group,
-    callback = function()
-        vim.highlight.on_yank()
-    end
-})

4d62d98202339a5c34fb92509d3ec7c304836910
Timothy Davis <tmthydvs@gmail.com>
2023-01-01 14:38:54 +0930

    Fix fish config ssh-agent

--- xdg_config/fish/config.fish
+++ xdg_config/fish/config.fish
@@ -1,6 +1,6 @@
 
 # Ensure ssh-agent is set
-if test -z (pgrep ssh-agent)
+if not pgrep ssh-agent | string collect > /dev/null
     eval (ssh-agent -c) > /dev/null
     set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
     set -Ux SSH_AGENT_PID $SSH_AGENT_PID

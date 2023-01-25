97b24b53bc8dad7834d5121d4a4c29ec7dc94db3
Timothy Davis <tmthydvs@gmail.com>
2023-01-25 12:45:20 +0930

    Minimal control center for eww

--- xdg_config/eww/scripts/network.fish
+++ xdg_config/eww/scripts/network.fish
@@ -1,11 +1,30 @@
 #!/usr/bin/env fish
 
-set -l output (nmcli -t -f SIGNAL,ACTIVE device wifi)
+function get_status
+    set -l output (nmcli -t -f SIGNAL,ACTIVE,SSID device wifi)
 
-for line in $output
-    set -l parts (string split ':' $line)
-    if test $parts[2] = "yes"
-        echo $parts[1]
-        break
+    for line in $output
+        set -l parts (string split ':' $line)
+        if test $parts[2] = "yes"
+            echo "{\"name\":\"$parts[3]\",\"strength\":$parts[1]}"
+            break
+        end
     end
+end
+
+function toggle
+    if test -z (get_status)
+        echo 'Enabling Wi-Fi'
+        nmcli radio wifi on
+    else
+        echo 'Disabling Wi-Fi'
+        nmcli radio wifi off
+    end
+end
+
+switch $argv[1]
+    case get
+        get_status
+    case toggle
+        toggle
 end
--- xdg_config/eww/scripts/volume.fish
+++ xdg_config/eww/scripts/volume.fish
@@ -1,5 +1,32 @@
 #!/usr/bin/env fish
 
-set -l output (amixer sget Master)
+function get_volume
+    set -l output (amixer sget Master)
 
-echo $output | awk -F"[][]" '/Left:/ { print $2 }'
+    set -l parts (echo $output | awk -F"[][]" '/Left:/ { print $2; print $4 }')
+
+    set -l level (string sub $parts[1] -e -1)
+    if test "$parts[2]" = "on"
+        set muted "false"
+    else
+        set muted "true"
+    end
+    echo "{\"muted\":$muted,\"level\":\"$level\"}"
+end
+
+function set_volume
+    amixer sset Master "$argv[1]%" > /dev/null
+end
+
+function toggle_mute
+    amixer sset Master toggle > /dev/null
+end
+
+switch $argv[1]
+    case get
+        get_volume
+    case set
+        set_volume $argv[2]
+    case mute
+        toggle_mute
+end

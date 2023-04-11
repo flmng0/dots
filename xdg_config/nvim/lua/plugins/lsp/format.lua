cefe9f454c9a3b9b9a03a44fa22f6dcc6bc6e6c7
Timothy Davis <tmthydvs@gmail.com>
2023-04-11 10:16:07 +0930

    Only create formatters one time

--- xdg_config/nvim/lua/plugins/lsp/format.lua
+++ xdg_config/nvim/lua/plugins/lsp/format.lua
@@ -1,24 +1,50 @@
 local M = {}
 
-M.format = function(buf)
-    local ft = vim.bo[buf].filetype
-    local nls_available = require('null-ls.sources').get_available(ft, 'NULL_LS_FORMATTING')
+local formatters = {}
+
+local make_formatter = function(ft)
+    local nls_sources = require('null-ls.sources')
+    local nls_methods = require('null-ls.methods')
+    local nls_available = nls_sources.get_available(ft, nls_methods.internal.FORMATTING)
+        or nls_sources.get_available(ft, nls_methods.lsp.FORMATTING)
     local have_nls = #nls_available > 0
 
-    vim.lsp.buf.format {
-        bufnr = buf,
-        filter = function(client)
-            if have_nls then
-                return client.name == 'null-ls'
-            end
-            return client.name ~= 'null-ls'
-        end,
-    }
+    local filter = function(client)
+        if have_nls then
+            return client.name == 'null-ls'
+        end
+
+        return client.name ~= 'null-ls'
+    end
+
+    return function(buf)
+        vim.lsp.buf.format {
+            bufnr = buf,
+            filter = filter,
+        }
+    end
+end
+
+M.format = function(buf)
+    local ft = vim.bo[buf].filetype
+    local format = formatters[ft]
+
+    if format == nil then
+        format = make_formatter(ft)
+        formatters[ft] = format
+    end
+
+    format(buf)
 end
 
 M.on_attach = function(client, buf)
     if not client.supports_method('textDocument/formatting') then
         return
+    end
+
+    local ft = vim.bo[buf].filetype
+    if formatters[ft] == nil then
+        formatters[ft] = make_formatter(ft)
     end
 
     vim.api.nvim_create_autocmd('BufWritePre', {

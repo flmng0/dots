9af05f6c99bd430f161acc8d032c9142099ace63
Timothy Davis <tmthydvs@gmail.com>
2024-03-30 16:31:44 +0930

    Use stow for dotfile management

--- install.sh
+++ install.sh
@@ -1,18 +1,37 @@
 #!/usr/bin/env nix-shell
-#! nix-shell -i zsh --pure
-#! nix-shell -p zsh
+#! nix-shell -i zsh -p zsh stow
 
-INSTALLERS=$(find . -mindepth 2 -type f -name install.sh -perm /+x)
+CURRENT_DIR=${0:A:h}
+
+nixos() {
+  NIXOS_CONFIG_PATH="/etc/nixos/configuration.nix"
+  NIXOS_LINK=$(readlink "$NIXOS_CONFIG_PATH")
+
+  echo "$CURRENT_DIR"
+
+  if [ $? -eq 0 ]; 
+  then
+    echo ":: NixOS configuration already linked, skipping..."
+    return
+  fi
+
+  echo ":: Linking NixOS configuration"
+  
+  sudo mv "$CONFIG_PATH" "/tmp/nixos-configuration.old.nix"
+  sudo ln -s "$CURRENT_DIR/nixos/configuration.nix" "$CONFIG_PATH"
+
+  echo ":: Rebuilding NixOS"
+
+  sudo nixos-rebuild switch > "$PWD/install.log"
+
+  echo ":: Rebuilt NixOS, find installation log at $PWD/install.log"
+  echo ":: Old NixOS configuration can be found at /tmp/nixos-configuration.old.nix"
+}
+
+nixos
 
 echo ":: Installing application configurations"
 
-for installer in $INSTALLERS;
-do
-  echo ":: Running installer script: $installer"
-  $installer
-done
+stow home
 
-echo ":: Done!"
-
-echo ":: Installer output can be found in '$PWD/install.log'"
 

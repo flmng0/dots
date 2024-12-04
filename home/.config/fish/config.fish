e408e1c1a51c2961ce59b67a1157632c0a2c69cb
Timothy Davis <tmthydvs@gmail.com>
2024-12-04 23:19:22 +0930

    Add bun and deno paths

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+fish_add_path $HOME/.deno/bin
+fish_add_path $HOME/.bun/bin
 fish_add_path $HOME/.local/bin
 source $HOME/.cargo/env.fish
 

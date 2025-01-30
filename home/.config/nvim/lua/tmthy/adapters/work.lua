ea8f4a21e67142a629dd3176d8071be16b555de0
Tim Davis <tim.davis@radicalsystems.com.au>
2025-01-30 12:46:23 +0930

    Make adapters properly wait for installation

--- home/.config/nvim/lua/tmthy/adapters/work.lua
+++ home/.config/nvim/lua/tmthy/adapters/work.lua
@@ -1,27 +1,32 @@
 require('tmthy.util').mason_ensure_installed({ 'php-debug-adapter' })
 
-local registry = require('mason-registry')
-local php_dap_root = registry.get_package('php-debug-adapter'):get_install_path()
-local php_dap_path = php_dap_root .. '/extension/out/phpDebug.js'
-
-return {
-	adapters = {
-		php = {
-			type = 'executable',
-			command = 'node',
-			args = { php_dap_path },
-		},
-	},
-
-	configurations = {
-		php = {
-			{
-				type = 'php',
-				request = 'launch',
-				-- Marked as global so it is easy to tell when it's not from .vscode/launch.json
-				name = '[Global] Listen for Xdebug',
-				port = 9003,
-			},
-		},
-	},
+local output = {
+	adapters = {},
+	configurations = {}
 }
+
+local registry = require('mason-registry')
+
+local has_php = registry.is_installed('php-debug-adapter')
+if has_php then
+	local php_dap_root = registry.get_package('php-debug-adapter'):get_install_path()
+	local php_dap_path = php_dap_root .. '/extension/out/phpDebug.js'
+
+	output.adapters['php'] = {
+		type = 'executable',
+		command = 'node',
+		args = { php_dap_path },
+	}
+
+	output.configurations['php'] = {
+		{
+			type = 'php',
+			request = 'launch',
+			-- Marked as global so it is easy to tell when it's not from .vscode/launch.json
+			name = '[Global] Listen for Xdebug',
+			port = 9003,
+		}
+	}
+end
+
+return output
--- home/.config/nvim/lua/tmthy/servers/work.lua
+++ home/.config/nvim/lua/tmthy/servers/work.lua
@@ -1,19 +1,7 @@
-local mason_registry = require('mason-registry')
-local vue_lsp_root = mason_registry.get_package('vue-language-server'):get_install_path()
-local vue_lsp_path = vue_lsp_root .. '/node_modules/@vue/language-server'
-
-return {
+local servers = {
 	omnisharp = {},
 	ts_ls = {
-		init_options = {
-			plugins = {
-				{
-					name = '@vue/typescript-plugin',
-					location = vue_lsp_path,
-					languages = { 'vue' },
-				},
-			},
-		},
+		init_options = {},
 		filetypes = {
 			'javascript',
 			'typescript',
@@ -24,3 +12,22 @@ return {
 	},
 	volar = {},
 }
+
+local mason_registry = require('mason-registry')
+local has_vue = mason_registry.is_installed('vue-language-server')
+if has_vue then
+	local vue_lsp_root = mason_registry.get_package('vue-language-server'):get_install_path()
+	local vue_lsp_path = vue_lsp_root .. '/node_modules/@vue/language-server'
+
+	servers.ts_ls['init_options'] = {
+		plugins = {
+			{
+				name = '@vue/typescript-plugin',
+				location = vue_lsp_path,
+				languages = { 'vue' },
+			},
+		},
+	}
+end
+
+return servers

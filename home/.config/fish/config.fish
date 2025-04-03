4e539c471f350c830b68ea0b9c024ce87d2a92ad
Timothy Davis <tmthydvs@gmail.com>
2025-04-03 17:35:14 +0930

    Add neovim in /opt to path

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+fish_add_path /opt/nvim-linux-x86_64/bin
 fish_add_path $HOME/.deno/bin
 fish_add_path $HOME/.bun/bin
 fish_add_path $HOME/.local/bin

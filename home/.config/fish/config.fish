7b68943a57b8ce17e81fa982123ad1f58436903a
Timothy Davis <tmthydvs@gmail.com>
2025-09-03 16:11:58 +0930

    Remove deno

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -3,10 +3,12 @@ if status is-interactive
 end
 
 fish_add_path /opt/nvim-linux-x86_64/bin
-fish_add_path $HOME/.deno/bin
 fish_add_path $HOME/.bun/bin
 fish_add_path $HOME/.local/bin
 source $HOME/.cargo/env.fish
+
+set -x FLYCTL_INSTALL "/home/tmthy/.fly"
+fish_add_path "$FLYCTL_INSTALL/bin"
 
 mise activate fish | source
 starship init fish | source
